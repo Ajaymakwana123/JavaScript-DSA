7c8cb2774ae3e86029800e877e5f5814fa1fe1ba
Ajaymakwana123 <ammakwana4150@gmail.com>
2025-06-23 18:27:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let a = 20;
+let b = 10;
+console.log(a + b + " is answer is 30");
+console.log(" is answer is 30" + (a + b));
+// priority bracket ne aapvama aave che
+
+console.log("1" - 1);
+// aama js substrack j karshe because "-" nu ek j kam che etl js string ne integer ma convert krine result print krshe :: aane ` type coercion ` kevay. " * / " ma same j work krashe 
+
